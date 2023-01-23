60139fa6c0d75b4a3e5e24a3a6db4a9c36a4bdc5
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-01-23 09:02:19 +0530

    error in program fixed

--- RotateArray.java
+++ RotateArray.java
@@ -3,7 +3,7 @@ package methodLearn;
 public class RotateArray {
 	public static void main(String[] args) {
 		int [] array = {1,4,5,5,7};
-		int n = array.length-1;
+		int n = array.length;
 		int temp=array[n];
 	    for(int i=(n-1);i>0;i--)	{
 	    	array[i]=array[i-1];
